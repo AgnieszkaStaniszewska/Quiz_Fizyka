b90ab31ed24725dd80ee2c0745f09dedfa0592d9
Agnieszka Staniszewska <>
2022-10-10 14:10:11 +0200

    Quiz z fizyki Java

---
+++ src/main/java/com/mycompany/staniszewskaa_quiz/StaniszewskaA_Quiz.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.staniszewskaa_quiz;
+
+import java.io.FileNotFoundException;
+
+/**
+ *
+ * @author agnieszka.cupryna
+ */
+public class StaniszewskaA_Quiz {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        //System.out.println("Hello World!");
+        new Start().setVisible(true);
+    }
+}
